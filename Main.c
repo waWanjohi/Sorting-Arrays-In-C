850e696a6001f8ef16325155ea42a682c244f517
waWanjohi <31609389+waWanjohi@users.noreply.github.com>
2018-02-02 10:26:01 +0300

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,39 @@
+//include the standard input-output library
+
+#include<stdio.h>
+
+ //The main function must be there
+int main(void){
+
+    int a[10],i=0,j=0,n,t;
+    printf("\n Enter the number of elements:");
+    scanf("%d", &n);
+    printf("\n");
+            for(i=0;i<n;i++){
+              printf("Enter the element%d:",(i+1));
+              scanf("%d",&a[i]);
+            }
+            for(j=0;j<(n-1);j++)
+            {
+            for(i=0;i<(n-1);i++)
+            {
+            for(i=0;i<(n-1);i++)
+            {if(a[i+1]<a[i])
+
+            {t=a[i];
+            a[i+1]=t;}}}
+
+                printf("\n Ascending order:");
+                for(i-0;i<n;i++){
+                printf("%d",a[i]);
+            }
+
+                printf("\n Descending order:");
+                for(i=n;i>0;i--){
+                printf("%d",a[i-1]);
+            }
+             return 0;
+       }   
+ 
+ }
+
