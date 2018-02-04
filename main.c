4a6be6ee5eaef24b617ce4aa2014c85189008c4b
Gideon Wanjohi <31609389+waWanjohi@users.noreply.github.com>
2018-02-04 12:52:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+//Find an even or odd number
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+            int a;
+
+            printf("Enter Any Number: \n");
+            scanf("%d", &a);
+            printf("--------------------- \n");
+
+            if(a%2==0){
+
+                    printf("\nNumber is even.");
+
+            }  else{
+
+                printf("\nNumber is odd");
+            }
+printf("'                 _      \n ");
+printf("     ___     /  /_       \n");
+printf("   /   _   \ /  __  \     \n");
+printf("  /   /_/   /  /   /  /     \n");
+printf("  \ ____ /_/   /_/      \n ");
+printf("                                 \n ");
+printf("                                     \n ");
+printf("                              \n    ");
+            return 0;
+}
